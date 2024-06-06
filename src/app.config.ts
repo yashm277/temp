ed6fd9f33787b011f929598c7406a0ed95c6234d
yashm277 <f20210001@pilani.bits-pilani.ac.in>
2024-06-07 01:38:39 +0530

    only chat

--- src/app.config.ts
+++ src/app.config.ts
@@ -12,10 +12,6 @@ import "./config/auth";
 // Import demo room handlers
 import { LobbyRoom, RelayRoom } from 'colyseus';
 import { ChatRoom } from "./rooms/01-chat-room";
-import { StateHandlerRoom } from "./rooms/02-state-handler";
-import { AuthRoom } from "./rooms/03-auth";
-import { ReconnectionRoom } from './rooms/04-reconnection';
-import { CustomLobbyRoom } from './rooms/07-custom-lobby-room';
 
 export default config({
     options: {
@@ -39,21 +35,6 @@ export default config({
         gameServer.define("chat_with_options", ChatRoom, {
             custom_options: "you can use me on Room#onCreate"
         });
-
-        // Define "state_handler" room
-        gameServer.define("state_handler", StateHandlerRoom)
-            .enableRealtimeListing();
-
-        // Define "auth" room
-        gameServer.define("auth", AuthRoom)
-            .enableRealtimeListing();
-
-        // Define "reconnection" room
-        gameServer.define("reconnection", ReconnectionRoom)
-            .enableRealtimeListing();
-
-        // Define "custom_lobby" room
-        gameServer.define("custom_lobby", CustomLobbyRoom);
 
         gameServer.onShutdown(function(){
             console.log(`game server is going down.`);
